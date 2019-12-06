06db692a4b29ead949348a42d0d38d85153493b7
Veronica Kemp <veronicakemp@Veronicas-MacBook-Pro.local>
2019-12-06 13:34:15 -0500

    dom illistration

--- dom.js
+++ dom.js
@@ -1,5 +1,38 @@
+// NOT REAL DOM, ILLISTRATED ON HOW A BROWSER MIGHT KEEP UP WITH ELEMENTS ON PAGE
+
 const document = {
     doctype: "html",
-    children:
-
-};
+    children: [
+        {
+            type: "html",
+            children: [
+                {
+                    type: "head",
+                    children: [
+                        {type: "meta"},
+                        {type: "meta"},
+                        {type: "title"}
+                    ]
+                },
+                {
+                    type: "body",
+                    children: [
+                        {
+                            type: "img",
+                            properties: [
+                                {
+                                    type: "class",
+                                    values: [
+                                        "js-picture"
+                                    ]
+                                    },
+                                    {
+                                        type: "src"
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
